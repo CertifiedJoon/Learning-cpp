c89ed31b7d908bd692eee1f1536a8f724db58f8a
CertifiedJoon <61619422+CertifiedJoon@users.noreply.github.com>
2021-02-20 11:13:45 +0900

    Create Sudoky_pseudo.cpp

---
+++ Recursion/Sudoky_pseudo.cpp
@@ -0,0 +1,16 @@
+bool SolveSudoku(grid<int> & grid)
+{
+	int row, col;
+	
+	if(FindUnassignedLocation(grid, row, col))
+		return true; //all slots assigned;
+	
+	for (int num = 1; nums <= 9; num++){
+		if (NoConflicts(gric, row, col, num)){
+			grid(row, col) = num;
+			if(SolveSudoku(grid)) return true;
+			grid(row, col) = UNASSIGNED;
+		}
+	}
+	return false;
+}
