08532582fb63375e2aa453dd99ef72d727aabf20
CertifiedJoon <61619422+CertifiedJoon@users.noreply.github.com>
2021-02-20 11:10:21 +0900

    Create QueensProblem_pseudo.cpp

---
+++ Recursion/QueensProblem_pseudo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "simple.h"
+#include "lexicon.h"
+
+bool Solve(Grid<bool> &board, int col){
+	if (col > board.numcols()) return true;
+	
+	for (int rowToTry = 0; rowToTry < board.numRows(); rowToTry++) {
+		if (IsSafe(board, RowToTrym col)){
+			PlaceQueen(board, rowToTry, col);
+			if (Solve(board, col + 1)) return true;
+			RemoveQueen(boardm rowToTry,col);
+		}
+	}
+	return false;
+}
