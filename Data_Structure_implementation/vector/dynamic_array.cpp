4431530120adaa6679d05f618ba72c2974115ba5
CertifiedJoon <u3566665@connect.hku.hk>
2021-05-27 14:15:38 +0000

    added swap optimization:

--- Data_Structure_implementation/vector/dynamic_array.cpp
+++ Data_Structure_implementation/vector/dynamic_array.cpp
@@ -6,8 +6,16 @@
 #include <exception>
 #include <initializer_list>
 
+template<typename> class vector;
+template<typename T> bool operator==(const vector<T>&, const vector<T>&);
+template<typename T> bool operator<(const vector<T>&, const vector<T>&);
+template<typename T> void swap(vector<T>&, vector<T>&);
+
 template<typename T>
 class vector {
+friend bool operator==(const vector<T>&, const vector<T>&);
+friend bool operator<(const vector<T>&, const vector<T>&);
+friend voi swap(vector<T>&, vector<T>&);
 public:
 	vector() : elements(nullptr); first_free(nullptr), cap (nullptr) {}
 	vector(const vector<T> &v);
@@ -31,7 +39,7 @@ public:
 private:
 	
 	static std::allocator<T> alloc;
-	std::pair<T*, T*> alloc_n_copy (const T*, const T*);
+	std::pair<T*, T*> allocNCopy (const T*, const T*);
 	void free();
 	void reallocate();
 	void chk_n_alloc() {if (size() == capacity()) reallocate();}
@@ -40,5 +48,81 @@ private:
 	T *first_free;
 	T *cap;
 };
+
+template<typename T>
+std::allocator<T> vector<T>::alloc;
+
+template<typename T>
+std::pair<T*, T*> vector<T>::allocNCopy (const T *b, const T *e) {
+	auto newp = alloc.allocate(e - b);
+	return {newp, std::uninitialized_copy(b, e, data)};
+}
+
+template<typename T>
+void vector<T>::free() {
+	if (elements) 
+	{
+		for (auto it = first_free; it != elements;)
+			alloc.destroy(--it);
+		alloc.deallocate(elements, cap - elements);
+	}
+}
+
+template<typename T>
+void vector<T>::reallocate() {
+	size_t new_cap = size() ? 2*size() : 16;
+	auto first = alloc.allocate(new_cap);
+	auto last  = std::unitialized_copy(std::make_move_iterator(begin()), std::make_move_iterator(end()), first);
+	
+	free();
+	elemenst = first;
+	first_free = last;
+	cap = first + new_cap;
+}
+
+
+template<typename T>
+vector<T>::vector(const vector<T> &v) {
+	auto newp = allocNCopy(v.begin(), v.end());
+	free();
+	elements = newp.first;
+	first_free = cap = newp.second;
+}
+
+template<typename T>
+vector<T>::vector(std::initializer_list<T> li) {
+	auto newp = allocNCopy(li.begin(), li.end());
+	elements = newp.first;
+	first_free = cap = newp.second;
+}
+
+template<typename T>
+void swap(vector<T> &lhs, vector<T> &rhs)
+{
+	using std::swap;
+	swap(lhs.elements, rhs.elements);
+	swap(lhs.first_free, rhs.first_free);
+	swap(lhs.cap, rhs.cap);
+}
+
+template<typename T>
+vector<T>& operator=(vector<T> rhs)  // we do not want to change rhs so we copy it to operator function then pass its reference to swap
+{
+	swap(*this, rhs);
+	return *this
+}
+
+template<typename T>
+vector<T>& operator=(std::initializer_list<T> li) {
+	auto newp = allocNCopy(li.begin(), li.end());
+	free();
+	elements = newp.first;
+	first_free = cap = newp.second;
+	return *this;
+}
+
+template<typename T>
+
+
 
 #endif
