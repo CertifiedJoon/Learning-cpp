dfeed0414cc1b39284997ad4515b977ea7081b24
Joon_Thinkpad <mjypark1212@gmail.com>
2023-01-06 13:59:17 +0400

    some playground work

---
+++ Goodrich/playground.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+using namespace std;
+using std::string;
+
+struct Student {
+  string _id;
+  string name;
+  int year;
+};
+
+typedef int Sales;
+
+int main() {
+  return EXIT_SUCCESS;
+}
